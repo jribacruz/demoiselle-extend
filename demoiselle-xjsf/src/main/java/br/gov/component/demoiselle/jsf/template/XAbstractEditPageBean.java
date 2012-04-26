6cea372853e0f8a10235d0e7a3c7e828f494b0bf
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-04-26 09:42:54 -0300

    first commit

---
+++ demoiselle-xjsf/src/main/java/br/gov/component/demoiselle/jsf/template/XAbstractEditPageBean.java
@@ -0,0 +1,12 @@
+package br.gov.component.demoiselle.jsf.template;
+
+import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
+
+
+public abstract class XAbstractEditPageBean<T, I> extends AbstractEditPageBean<T, I> {
+	private static final long serialVersionUID = 1L;
+
+	public abstract String create();
+
+	public abstract String edit(I id);
+}
---
+++ demoiselle-xjsf/src/main/java/br/gov/component/demoiselle/jsf/template/XAbstractListPageBean.java
@@ -0,0 +1,52 @@
+package br.gov.component.demoiselle.jsf.template;
+
+import java.util.List;
+import java.util.Map;
+
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+import br.gov.frameworkdemoiselle.pagination.Pagination;
+import br.gov.frameworkdemoiselle.template.AbstractListPageBean;
+
+public abstract class XAbstractListPageBean<T, I> extends AbstractListPageBean<T, I> {
+	private static final long serialVersionUID = 1L;
+	private LazyDataModel<T> lazyDataModel;
+
+	protected void initLazyDataModel() {
+		
+		this.lazyDataModel = new LazyDataModel<T>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public List<T> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
+				Pagination pagination = getPagination();
+				pagination.setFirstResult(first);
+				pagination.setPageSize(pageSize);
+								
+				List<T> list = handleResultList(sortField, sortOrder);
+				this.setRowCount(pagination.getTotalResults());
+				this.setPageSize(pageSize);
+								
+				return list;
+			}
+
+		};
+	}
+
+	protected abstract String deleteSelection();
+
+	protected abstract List<T> handleResultList(String sortField, SortOrder sortOrder);
+
+	public LazyDataModel<T> getLazyDataModel() {
+		if (lazyDataModel == null) {
+			initLazyDataModel();
+		}
+		return lazyDataModel;
+	}
+
+	public void setLazyDataModel(LazyDataModel<T> lazyDataModel) {
+		this.lazyDataModel = lazyDataModel;
+	}
+
+}
