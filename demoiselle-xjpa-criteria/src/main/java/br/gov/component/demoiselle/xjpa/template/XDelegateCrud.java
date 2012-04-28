a145cdaaa6fc8290b5e0b9213ecbf2422800eb45
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-04-28 19:16:59 -0300

    new findAll method

--- demoiselle-xjpa-criteria/src/main/java/br/gov/component/demoiselle/xjpa/template/XDelegateCrud.java
+++ demoiselle-xjpa-criteria/src/main/java/br/gov/component/demoiselle/xjpa/template/XDelegateCrud.java
@@ -1,14 +1,21 @@
 package br.gov.component.demoiselle.xjpa.template;
 
 import java.util.List;
+import java.util.Map;
 
+import br.gov.component.demoiselle.xjpa.CriteriaOrder;
 import br.gov.component.demoiselle.xjpa.Criterion;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
 
 public class XDelegateCrud<T, I, C extends XJPACrud<T, I>> extends DelegateCrud<T, I, XJPACrud<T, I>> {
 	private static final long serialVersionUID = 1L;
-	
-	public List<T> findAll(Class<? extends Criterion> criterion, int pageSize) {
+
+	public List<T> findAll(Class<? extends Criterion<T>> criterion, int pageSize) {
 		return this.getDelegate().findAll(criterion, pageSize);
 	}
+
+	public List<T> findAll(String sortField, CriteriaOrder order, Map<String, String> filters) {
+		return this.getDelegate().findAll(sortField, order, filters);
+	}
+
 }
