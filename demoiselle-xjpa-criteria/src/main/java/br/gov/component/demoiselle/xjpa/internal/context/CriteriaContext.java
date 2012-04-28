a306aad1e0f533a2a81980031acbecb12226fc0c
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-04-28 19:15:43 -0300

    new findAll method

--- demoiselle-xjpa-criteria/src/main/java/br/gov/component/demoiselle/xjpa/internal/context/CriteriaContext.java
+++ demoiselle-xjpa-criteria/src/main/java/br/gov/component/demoiselle/xjpa/internal/context/CriteriaContext.java
@@ -1,13 +1,18 @@
 package br.gov.component.demoiselle.xjpa.internal.context;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import javax.persistence.TypedQuery;
 
+import br.gov.component.demoiselle.xjpa.CriteriaOrder;
 import br.gov.component.demoiselle.xjpa.Criterion;
 
 public interface CriteriaContext extends Serializable {
+	@SuppressWarnings("rawtypes")
 	void criteria(Class<? extends Criterion> criteria);
+
+	<E> void criteria(Class<E> beanClass, CriteriaOrder criteriaOrder, String sortField, Map<String, String> filters);
 
 	void paginate(boolean flag);
 
@@ -17,12 +22,14 @@ public interface CriteriaContext extends Serializable {
 
 	void clearParams();
 
+	CriteriaOrder getOrder();
+
 	<T> TypedQuery<T> getQuery(Class<T> bean);
 
 	<T> TypedQuery<Long> getQueryCount(Class<T> bean);
 
-	void addCriteriaParam(String key, Object value);
+	void addParam(String key, Object value);
 
-	Object getCriteriaParam(String key);
+	Object getParam(String key);
 
 }
