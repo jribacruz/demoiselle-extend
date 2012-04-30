b6a71d57503626ff05e5c02d4c9443dec6b17b1a
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-04-30 12:42:20 -0300

    Message Annotation

---
+++ demoiselle-xvalidation/src/main/java/br/gov/component/demoiselle/xvalidation/annotation/Message.java
@@ -0,0 +1,20 @@
+package br.gov.component.demoiselle.xvalidation.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.Nonbinding;
+import javax.interceptor.InterceptorBinding;
+
+@Target(value = { ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@InterceptorBinding
+public @interface Message {
+	@Nonbinding
+	String success();
+
+	@Nonbinding
+	String failure();
+}
