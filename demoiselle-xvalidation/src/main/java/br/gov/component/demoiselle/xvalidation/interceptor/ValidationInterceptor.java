9315cd43a4a5c216b2458ae59ba6da24e94f661f
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-05-02 08:18:22 -0300

    refatoracao do metodo handle

--- demoiselle-xvalidation/src/main/java/br/gov/component/demoiselle/xvalidation/interceptor/ValidationInterceptor.java
+++ demoiselle-xvalidation/src/main/java/br/gov/component/demoiselle/xvalidation/interceptor/ValidationInterceptor.java
@@ -14,12 +14,11 @@ import br.gov.component.demoiselle.xvalidation.internal.context.ValidationContex
 import br.gov.frameworkdemoiselle.message.DefaultMessage;
 import br.gov.frameworkdemoiselle.message.Message;
 import br.gov.frameworkdemoiselle.message.MessageContext;
-import br.gov.frameworkdemoiselle.stereotype.Controller;
 import br.gov.frameworkdemoiselle.util.Faces;
 import br.gov.frameworkdemoiselle.util.Strings;
 
-@Controller
 @Interceptor
+@Validation(message = "")
 public class ValidationInterceptor {
 	@Inject
 	private MessageContext messageContext;
@@ -34,23 +33,28 @@ public class ValidationInterceptor {
 	public Object handler(InvocationContext context) throws Exception {
 		Method method = context.getMethod();
 		Validation validation = method.getAnnotation(Validation.class);
-		if (method != null && validation != null) {
-			logger.info("Chamada ao metodo de validacao: [" + method.getName() + "]");
-			Object ret = context.proceed();
+		logger.info("Chamada ao metodo de validacao: [" + method.getName() + "]");
+		Object ret = null;
+		try {
+			ret = context.proceed();
 			String message = validation.message();
 			String clientId = validation.clientId();
-			if (!(Boolean) ret) {
-				Message msg = new DefaultMessage(message, validationContext.getValidationParam());
-				if (!Strings.isEmpty(clientId)) {
-					Faces.addMessage(clientId, msg);
-				} else {
-					messageContext.add(msg);
+			if (!Strings.isEmpty(message)) {
+				if (!(Boolean) ret) {
+					Message msg = new DefaultMessage(message, validationContext.getValidationParam());
+					if (!Strings.isEmpty(clientId)) {
+						Faces.addMessage(clientId, msg);
+					} else {
+						messageContext.add(msg);
+					}
 				}
 			}
 			validationContext.addValidation((Boolean) ret);
 			return ret;
-		} else {
-			return context.proceed();
+		} catch (Exception e) {
+			validationContext.addValidation(false);
+			e.printStackTrace();
 		}
+		return ret;
 	}
 }
