7c8efba15d41bd9aa280a7873095a0454ed55734
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-05-02 08:19:57 -0300

    inclusao da annotation @nonbinding

--- demoiselle-xvalidation/src/main/java/br/gov/component/demoiselle/xvalidation/Validation.java
+++ demoiselle-xvalidation/src/main/java/br/gov/component/demoiselle/xvalidation/Validation.java
@@ -5,9 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(value = ElementType.METHOD)
+import javax.enterprise.util.Nonbinding;
+import javax.interceptor.InterceptorBinding;
+
+@Target(value = {ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@InterceptorBinding
 public @interface Validation {
+	@Nonbinding
 	String message();
+
+	@Nonbinding
 	String clientId() default "";
 }
