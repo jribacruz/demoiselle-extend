2da3f66ac7cd4e36410739ef356209d356746cd9
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-05-04 11:35:47 -0300

    ajuste no nome do pacote

---
+++ demoiselle-xjpa-criteria/src/main/java/br/gov/component/demoiselle/xjpa/context/CriteriaContext.java
@@ -0,0 +1,35 @@
+package br.gov.component.demoiselle.xjpa.context;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import javax.persistence.TypedQuery;
+
+import br.gov.component.demoiselle.xjpa.CriteriaOrder;
+import br.gov.component.demoiselle.xjpa.Criterion;
+
+public interface CriteriaContext extends Serializable {
+	@SuppressWarnings("rawtypes")
+	void criteria(Class<? extends Criterion> criteria);
+
+	<E> void criteria(Class<E> beanClass, CriteriaOrder criteriaOrder, String sortField, Map<String, String> filters);
+
+	void paginate(boolean flag);
+
+	boolean isPaginated();
+
+	void clearAll();
+
+	void clearParams();
+
+	CriteriaOrder getOrder();
+
+	<T> TypedQuery<T> getQuery(Class<T> bean);
+
+	<T> TypedQuery<Long> getQueryCount(Class<T> bean);
+
+	void addParam(String key, Object value);
+
+	Object getParam(String key);
+
+}
