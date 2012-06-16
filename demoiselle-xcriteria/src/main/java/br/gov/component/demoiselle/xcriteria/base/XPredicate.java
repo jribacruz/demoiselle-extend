c19cef87749558aef89c49169c200352c78f5d42
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-16 15:45:26 -0300

    alteracao no assinatura

--- demoiselle-xcriteria/src/main/java/br/gov/component/demoiselle/xcriteria/base/XPredicate.java
+++ demoiselle-xcriteria/src/main/java/br/gov/component/demoiselle/xcriteria/base/XPredicate.java
@@ -1,14 +1,13 @@
 package br.gov.component.demoiselle.xcriteria.base;
 
-import java.lang.annotation.Annotation;
-import java.util.List;
+import java.lang.reflect.Field;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Root;
 
 public interface XPredicate<T> {
-	public boolean check(List<Annotation> annotations, Class<?> fieldType, String fieldName, Object fieldValue);
+	public boolean check(Field field, Class<?> fieldType, String fieldName, Object fieldValue);
 
-	public <X> T apply(CriteriaBuilder cb, Root<X> p, String fieldName, Object fieldValue);
+	public <X> T apply(CriteriaBuilder cb, Root<X> p, Class<?> fieldType, String fieldName, Object fieldValue);
 
 }
