14dbc1883d514b930a7fb07969853a8731f5aaf2
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-26 19:49:07 -0300

    delegate JPACrud

--- demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/decorator/JPACrudDecorator.java
+++ demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/decorator/JPACrudDecorator.java
@@ -2,39 +2,30 @@ package br.gov.component.demoiselle.jsf.criteria.decorator;
 
 import java.util.List;
 
+import javax.decorator.Decorator;
 import javax.decorator.Delegate;
 import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 
-import br.gov.frameworkdemoiselle.template.Crud;
+import org.slf4j.Logger;
 
-public class JPACrudDecorator<T, I> implements Crud<T, I> {
+import br.gov.frameworkdemoiselle.template.JPACrud;
+
+@Decorator
+public class JPACrudDecorator<T, I> extends JPACrud<T, I> {
 	private static final long serialVersionUID = 1L;
 
 	@Inject
 	@Delegate
 	@Any
-	private Crud<T, I> dao;
+	private JPACrud<T, I> dao;
 
-	public void delete(I id) {
-		dao.delete(id);
-	}
+	@Inject
+	private Logger log;
 
+	@Override
 	public List<T> findAll() {
-		System.out.println("Interceptando a chamada do findAll()");
-		return null;
-	}
-
-	public void insert(T entity) {
-		dao.insert(entity);
-	}
-
-	public T load(I entity) {
-		return dao.load(entity);
-	}
-
-	public void update(T entity) {
-		dao.update(entity);
+		return dao.findAll();
 	}
 
 }
