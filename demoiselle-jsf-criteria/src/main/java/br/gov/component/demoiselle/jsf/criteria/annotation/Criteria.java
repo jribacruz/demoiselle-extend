2e594f73a27adb70a0035d1ae99112e64c8f3ca6
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-28 01:14:45 -0300

    definicao de interceptor

--- demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/annotation/Criteria.java
+++ demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/annotation/Criteria.java
@@ -5,10 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.enterprise.util.Nonbinding;
+import javax.interceptor.InterceptorBinding;
+
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = ElementType.METHOD)
+@Target(value = {ElementType.METHOD, ElementType.TYPE})
+@InterceptorBinding
 public @interface Criteria {
+	@Nonbinding
 	Class<?> value();
 
+	@Nonbinding
 	int pageSize() default 0;
 }
