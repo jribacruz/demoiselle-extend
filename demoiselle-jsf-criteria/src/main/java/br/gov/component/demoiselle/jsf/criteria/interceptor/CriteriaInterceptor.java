1c6fbbe4951ccb6e5ce3830ddcf3443889ab2522
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-28 01:16:25 -0300

    add do if

--- demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/interceptor/CriteriaInterceptor.java
+++ demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/interceptor/CriteriaInterceptor.java
@@ -5,20 +5,29 @@ import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
+import org.slf4j.Logger;
+
 import br.gov.component.demoiselle.jsf.criteria.annotation.Criteria;
 import br.gov.component.demoiselle.jsf.criteria.context.CriteriaContext;
 
 @Interceptor
+@Criteria(value = Void.class)
 public class CriteriaInterceptor {
 
 	@Inject
 	private CriteriaContext context;
 
+	@Inject
+	private Logger log;
+
 	@AroundInvoke
 	public Object handle(InvocationContext ctx) throws Exception {
 		Criteria criteria = ctx.getMethod().getAnnotation(Criteria.class);
-		context.setCriteria(criteria.value());
-		context.setPageSize(criteria.pageSize());
+		if (criteria != null) {
+			context.setCriteria(criteria.value());
+			context.setPageSize(criteria.pageSize());
+			log.info("Restriction selecionada: {}", new Object[] { criteria.value().getSimpleName() });
+		}
 		return ctx.proceed();
 	}
 
