06a9c52158f562c7c1977e81aa8d192291dbcef1
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-28 01:16:48 -0300

    add de annotations

--- demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/stereotype/CriteriaController.java
+++ demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/stereotype/CriteriaController.java
@@ -5,8 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.enterprise.inject.Stereotype;
+import javax.inject.Named;
+
+import br.gov.frameworkdemoiselle.annotation.ViewScoped;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE })
+@Stereotype
+@ViewScoped
+@Named
 public @interface CriteriaController {
 
 }
