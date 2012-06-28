2d8ab0fac571f3129a3b5667f91fab9816d32b22
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-06-28 01:16:38 -0300

    nova assinatura

--- demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/processor/CriteriaProcessor.java
+++ demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/processor/CriteriaProcessor.java
@@ -1,7 +1,8 @@
 package br.gov.component.demoiselle.jsf.criteria.processor;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface CriteriaProcessor extends Serializable {
-
+	public <T> List<T> getResultList(Class<T> beanClass);
 }
