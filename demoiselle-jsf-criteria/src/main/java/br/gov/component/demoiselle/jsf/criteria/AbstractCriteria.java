7cf99b5ba174c00fbab10a8664ef801270b71820
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-07-11 09:06:06 -0300

    add Serialize

--- demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/AbstractCriteria.java
+++ demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/AbstractCriteria.java
@@ -1,12 +1,15 @@
 package br.gov.component.demoiselle.jsf.criteria;
 
+import java.io.Serializable;
+
 import javax.inject.Inject;
 
 import org.primefaces.model.SortOrder;
 
 import br.gov.component.demoiselle.jsf.criteria.context.CriteriaContext;
 
-public abstract class AbstractCriteria<T> {
+public abstract class AbstractCriteria<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
 	private SortOrder sortOrder;
 	private String sortField;
 	private String query;
