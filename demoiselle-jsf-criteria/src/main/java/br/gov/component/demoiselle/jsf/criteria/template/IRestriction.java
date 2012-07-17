ec856359f8c5e52f92543453a6e6d2f6d4a09600
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-07-17 09:04:55 -0300

    refatoracao

--- demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/template/IRestriction.java
+++ demoiselle-jsf-criteria/src/main/java/br/gov/component/demoiselle/jsf/criteria/template/IRestriction.java
@@ -1,9 +1,12 @@
 package br.gov.component.demoiselle.jsf.criteria.template;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-public interface IRestriction<T> extends ICriteria<T>{
+public interface IRestriction<T> {
 	public Predicate restriction(CriteriaBuilder cb, Root<T> p);
+
+	public void projection(CriteriaBuilder cb, CriteriaQuery<T> cq, Root<T> p);
 }
