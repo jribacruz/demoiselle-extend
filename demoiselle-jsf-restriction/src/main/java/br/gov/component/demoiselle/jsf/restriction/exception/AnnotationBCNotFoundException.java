9d21ce3ce51d03cdf34ddaca0cc6317f926d71d9
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-07-31 20:05:44 -0300

    AnnotationException

---
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/exception/AnnotationBCNotFoundException.java
@@ -0,0 +1,13 @@
+package br.gov.component.demoiselle.jsf.restriction.exception;
+
+import br.gov.frameworkdemoiselle.DemoiselleException;
+
+public class AnnotationBCNotFoundException extends DemoiselleException {
+	private static final long serialVersionUID = 1L;
+
+	public AnnotationBCNotFoundException(String message) {
+		super(message);
+	}
+
+
+}
