350d538756b5f753bc0b44aa1b7f9625bf56ab44
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-02 08:51:28 -0300

    criteriaContext

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/producer/LazyDataModelProducer.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/producer/LazyDataModelProducer.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
 
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
@@ -15,7 +16,9 @@ import org.primefaces.model.SortOrder;
 import br.gov.component.demoiselle.jsf.restriction.AbstractCriteriaBean;
 import br.gov.component.demoiselle.jsf.restriction.annotation.BC;
 import br.gov.component.demoiselle.jsf.restriction.annotation.Criteria;
+import br.gov.component.demoiselle.jsf.restriction.context.CriteriaContext;
 import br.gov.component.demoiselle.jsf.restriction.exception.AnnotationBCNotFoundException;
+import br.gov.component.demoiselle.jsf.restriction.exception.AnnotationCriteriaNotFoundException;
 import br.gov.frameworkdemoiselle.pagination.Pagination;
 import br.gov.frameworkdemoiselle.template.AbstractListPageBean;
 import br.gov.frameworkdemoiselle.template.DelegateCrud;
@@ -24,22 +27,32 @@ import br.gov.frameworkdemoiselle.util.Beans;
 public class LazyDataModelProducer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private CriteriaContext criteriaContext;
+
 	@SuppressWarnings("rawtypes")
 	@Produces
 	@Default
 	public <T> LazyDataModel getLazyDataModel(final InjectionPoint ip) {
 		final Field field = (Field) ip.getMember();
+		validateBCPresence(field);
+		validateCriteriaPresence(field);
 		final AbstractListPageBean listPageBean = getListMB(ip);
-		validateBCpresence(field);
 		final DelegateCrud bc = getBC(field);
-		final AbstractCriteriaBean criteria = getCriteria(field);
+		final Class<? extends AbstractCriteriaBean> criteria = getCriteriaClass(field);
 		System.out.println("Produzindo a listagem....");
 		return getLazyDataModelInstance(bc, listPageBean, criteria);
 	}
 
-	private void validateBCpresence(Field field) {
+	private void validateBCPresence(Field field) {
 		if (!field.isAnnotationPresent(BC.class)) {
 			throw new AnnotationBCNotFoundException("Annotation @BC não encontrada no campo: " + field.getName());
+		}
+	}
+
+	private void validateCriteriaPresence(Field field) {
+		if (!field.isAnnotationPresent(Criteria.class)) {
+			throw new AnnotationCriteriaNotFoundException("Annotation @Criteria não encontrada no campo: " + field.getName());
 		}
 	}
 
@@ -54,28 +67,26 @@ public class LazyDataModelProducer implements Serializable {
 	}
 
 	@SuppressWarnings("rawtypes")
-	private AbstractCriteriaBean getCriteria(Field field) {
-		if (field.isAnnotationPresent(Criteria.class)) {
-			return Beans.getReference(field.getAnnotation(Criteria.class).value());
-		}
-		return null;
+	private Class<? extends AbstractCriteriaBean> getCriteriaClass(Field field) {
+		return field.isAnnotationPresent(Criteria.class) ? field.getAnnotation(Criteria.class).value() : null;
 	}
 
 	@SuppressWarnings("rawtypes")
 	private <T> LazyDataModel<T> getLazyDataModelInstance(final DelegateCrud bc, final AbstractListPageBean listMB,
-			final AbstractCriteriaBean criteria) {
+			final Class<? extends AbstractCriteriaBean> criteriaBeanClass) {
 		return new LazyDataModel<T>() {
 			private static final long serialVersionUID = 1L;
 
 			@SuppressWarnings("unchecked")
 			@Override
 			public List<T> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
-				if (criteria != null) {
-					criteria.setFilter(filters);
-					criteria.setSortField(sortField);
-					criteria.setSortOrder(sortOrder);
-					System.out.println("Setando os criterios: ");
-				}
+
+				criteriaContext.setFilters(filters);
+				criteriaContext.setSortField(sortField);
+				criteriaContext.setSortOrder(sortOrder);
+				criteriaContext.setCriteriaControllerClass(criteriaBeanClass);
+				System.out.println("Setando bean class: " + criteriaBeanClass.getCanonicalName());
+
 				Pagination pagination = listMB.getPagination();
 				pagination.setFirstResult(first);
 				pagination.setPageSize(pageSize);
