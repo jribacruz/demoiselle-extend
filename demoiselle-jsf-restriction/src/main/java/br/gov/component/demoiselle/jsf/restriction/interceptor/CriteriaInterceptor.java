1ff381cd1c95f8b4507c0d173a86d5dbbabeab16
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-03 09:34:24 -0300

    suporte a autocomplete

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/interceptor/CriteriaInterceptor.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/interceptor/CriteriaInterceptor.java
@@ -33,13 +33,38 @@ public class CriteriaInterceptor implements Serializable {
 	@SuppressWarnings("rawtypes")
 	@AroundInvoke
 	public Object handle(InvocationContext ctx) throws Exception {
-		Class<? extends AbstractCriteriaBean> criteriaClass = context.getCriteriaControllerClass();
-		Class<? extends AbstractCriteriaBean> criteriaSelectedClass = ctx.getMethod().getAnnotation(Criteria.class).value();
+		Class<? extends AbstractCriteriaBean> criteriaClass = getContextCriteriaBeanClass();
+		Class<? extends AbstractCriteriaBean> criteriaSelectedClass = getCriteriaBeanClass(ctx);
+		int pageSize = getPageSize(ctx);
 		if (criteriaClass != criteriaSelectedClass) {
 			context.setCriteriaControllerClass(criteriaSelectedClass);
-			logger.info("Criteria {} no método {}", criteriaSelectedClass.getCanonicalName() , ctx.getMethod().getName());
+			context.setPageSize(pageSize);
+			setQueryParamIfNecessary(ctx);
+			logger.info("Criteria selecionado {} no método {}", criteriaSelectedClass.getCanonicalName(), ctx.getMethod()
+					.getName());
 		}
-
 		return ctx.proceed();
+	}
+
+	@SuppressWarnings({ "rawtypes" })
+	private Class<? extends AbstractCriteriaBean> getContextCriteriaBeanClass() {
+		return context.getCriteriaControllerClass();
+	}
+
+	@SuppressWarnings("rawtypes")
+	private Class<? extends AbstractCriteriaBean> getCriteriaBeanClass(InvocationContext ctx) {
+		return ctx.getMethod().getAnnotation(Criteria.class).value();
+	}
+
+	private int getPageSize(InvocationContext ctx) {
+		return ctx.getMethod().getAnnotation(Criteria.class).pageSize();
+	}
+
+	private void setQueryParamIfNecessary(InvocationContext ctx) {
+		if (ctx.getParameters() != null && ctx.getParameters().length > 0) {
+			if (ctx.getParameters()[0].getClass() == String.class) {
+				context.setQuery((String) ctx.getParameters()[0]);
+			}
+		}
 	}
 }
