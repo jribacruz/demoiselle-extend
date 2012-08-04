b9bae0801951419472bfb02238a53d4bfb4788c5
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-04 12:44:20 -0300

    restriction annotation

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
@@ -1,15 +1,16 @@
-package br.gov.component.demoiselle.jsf.restriction.qualifier;
+package br.gov.component.demoiselle.jsf.restriction.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.inject.Qualifier;
+import br.gov.component.demoiselle.jsf.restriction.template.RestrictionBean;
 
-@Qualifier
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value = { ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
 public @interface Restriction {
-	String value();
+
+	@SuppressWarnings("rawtypes")
+	Class<? extends RestrictionBean> value();
 }
