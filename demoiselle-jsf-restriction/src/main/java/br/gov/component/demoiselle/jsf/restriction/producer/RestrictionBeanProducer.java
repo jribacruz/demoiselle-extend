848242f6407605f785abc04d725c8c98440cecbe
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-04 12:44:56 -0300

    RestrictionBeanProducer

---
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/producer/RestrictionBeanProducer.java
@@ -0,0 +1,25 @@
+package br.gov.component.demoiselle.jsf.restriction.producer;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import br.gov.component.demoiselle.jsf.restriction.annotation.Restriction;
+import br.gov.component.demoiselle.jsf.restriction.template.RestrictionBean;
+import br.gov.frameworkdemoiselle.util.Beans;
+
+public class RestrictionBeanProducer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@SuppressWarnings("unchecked")
+	public <T,X> RestrictionBean<T, X> create(InjectionPoint ip) {
+		Field field= (Field) ip.getMember();
+		if(field.isAnnotationPresent(Restriction.class)) {
+			RestrictionBean<T,X> bean = Beans.getReference(field.getAnnotation(Restriction.class).value());
+			return bean;
+		}
+		return null;
+	}
+
+}
