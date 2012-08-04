27fd6076b2b26367c6288ebc255da20870f2c6e0
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-04 12:46:45 -0300

    ElementType

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 import br.gov.component.demoiselle.jsf.restriction.template.RestrictionBean;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = { ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER })
+@Target(value = { ElementType.FIELD })
 public @interface Restriction {
 
 	@SuppressWarnings("rawtypes")
