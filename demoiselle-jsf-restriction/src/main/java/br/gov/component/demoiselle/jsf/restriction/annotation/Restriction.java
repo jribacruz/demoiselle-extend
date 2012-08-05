689bd4d88216b73849e2fb15700188cad0085139
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-05 10:00:23 -0300

    to qualifier

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
@@ -5,12 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+
 import br.gov.component.demoiselle.jsf.restriction.template.RestrictionBean;
 
+@Qualifier
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = { ElementType.FIELD })
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 public @interface Restriction {
-
+	@Nonbinding
 	@SuppressWarnings("rawtypes")
 	Class<? extends RestrictionBean> value();
 }
--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/producer/RestrictionBeanProducer.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/producer/RestrictionBeanProducer.java
@@ -3,6 +3,7 @@ package br.gov.component.demoiselle.jsf.restriction.producer;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 
+import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
 import br.gov.component.demoiselle.jsf.restriction.annotation.Restriction;
@@ -13,6 +14,8 @@ public class RestrictionBeanProducer implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@SuppressWarnings("unchecked")
+	@Produces
+	@Restriction(RestrictionBean.class)
 	public <T,X> RestrictionBean<T, X> create(InjectionPoint ip) {
 		Field field= (Field) ip.getMember();
 		if(field.isAnnotationPresent(Restriction.class)) {
