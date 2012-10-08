0f9d2d4892dd7f679e052fb9797a6b348cf54e37
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-10-08 14:45:43 -0300

    refatoracao para utilizacao do metodo invokeMethodReturnCollection

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/impl/CriteriaProcessorContextImpl.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/impl/CriteriaProcessorContextImpl.java
@@ -1,29 +1,22 @@
 package br.gov.component.demoiselle.jsf.restriction.impl;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.apache.commons.beanutils.MethodUtils;
-import org.slf4j.Logger;
-
 import br.gov.component.demoiselle.jsf.restriction.AbstractCriteriaBean;
 import br.gov.component.demoiselle.jsf.restriction.context.CriteriaProcessorContext;
+import br.gov.component.demoiselle.jsf.restrictions.util.Utils;
 import br.gov.frameworkdemoiselle.util.Beans;
 
 @SessionScoped
 public class CriteriaProcessorContextImpl implements CriteriaProcessorContext {
 	private static final long serialVersionUID = 1L;
-
-	@Inject
-	private Logger logger;
 
 	@SuppressWarnings("rawtypes")
 	private Class<? extends AbstractCriteriaBean> criteriaBeanClass;
@@ -31,7 +24,6 @@ public class CriteriaProcessorContextImpl implements CriteriaProcessorContext {
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void setCriteriaControllerClass(Class<? extends AbstractCriteriaBean> criteriaBeanClass) {
-		logger.info("Criteria {}", criteriaBeanClass.getName());
 		this.criteriaBeanClass = criteriaBeanClass;
 	}
 
@@ -46,37 +38,12 @@ public class CriteriaProcessorContextImpl implements CriteriaProcessorContext {
 	public <T> List<Predicate> getPredicates(CriteriaBuilder cb, Root<T> p) {
 		if (criteriaBeanClass != null) {
 			AbstractCriteriaBean<T> bean = Beans.getReference(criteriaBeanClass);
-			return bean != null ? invokePredicateList(bean, cb, p) : new ArrayList<Predicate>();
+
+			if (bean != null) {
+				return Utils.<Predicate> invokeMethodReturnCollection(bean, "getPredicates");
+			}
 		}
 		return new ArrayList<Predicate>();
-	}
-
-	@SuppressWarnings("unchecked")
-	private <T> List<Predicate> invokePredicateList(AbstractCriteriaBean<T> bean, CriteriaBuilder cb, Root<T> p) {
-		try {
-			return (List<Predicate>) MethodUtils.invokeMethod(bean, "getPredicateList", new Object[] { cb, p });
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		return new ArrayList<Predicate>();
-	}
-
-	@SuppressWarnings("unchecked")
-	public <T> List<Order> invokeOrderList(AbstractCriteriaBean<T> bean, CriteriaBuilder cb, Root<T> p) {
-		try {
-			return (List<Order>) MethodUtils.invokeMethod(bean, "getOrder", new Object[] { cb, p });
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		return new ArrayList<Order>();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -84,8 +51,9 @@ public class CriteriaProcessorContextImpl implements CriteriaProcessorContext {
 	public <T> List<Order> getOrders(CriteriaBuilder cb, Root<T> p) {
 		if (criteriaBeanClass != null) {
 			AbstractCriteriaBean<T> bean = Beans.getReference(criteriaBeanClass);
+
 			if (bean != null) {
-				return invokeOrderList(bean, cb, p);
+				return Utils.<Order> invokeMethodReturnCollection(bean, "getOrders", cb, p);
 			}
 		}
 		return new ArrayList<Order>();
