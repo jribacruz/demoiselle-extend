51bb53fab7b479839d0d5541a05c229fd5c68f34
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-10-09 20:08:15 -0300

    add do atributo optional

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/Restriction.java
@@ -5,16 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.enterprise.util.Nonbinding;
-import javax.inject.Qualifier;
-
-import br.gov.component.demoiselle.jsf.restriction.template.RestrictionBean;
-
-@Qualifier
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Target(value = { ElementType.FIELD, ElementType.METHOD })
 public @interface Restriction {
-	@Nonbinding
-	@SuppressWarnings("rawtypes")
-	Class<? extends RestrictionBean> value();
+	boolean optional() default true;
 }
