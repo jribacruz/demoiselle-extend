0deebfb536359472bf48de7a597580c864def601
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-10-15 23:11:55 -0300

    outras custom restrictions

---
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/custom/MemberRestrictionBean.java
@@ -0,0 +1,19 @@
+package br.gov.component.demoiselle.jsf.restriction.custom;
+
+import java.util.Collection;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import br.gov.component.demoiselle.jsf.restriction.template.RestrictionBean;
+
+public class MemberRestrictionBean<T, X> extends RestrictionBean<T, X> {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Predicate restriction(CriteriaBuilder cb, Root<T> p) {
+		return cb.isMember(getValue(), p.<Collection<X>> get(getField()));
+	}
+
+}
