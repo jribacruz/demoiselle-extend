08d5e624a51edb6f1bb979158323db294d021c67
jribacruz <jribacruz@gmail.com>
2012-11-20 00:23:08 -0300

    fix das annotations

--- demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/PredicateGroup.java
+++ demoiselle-jsf-restriction/src/main/java/br/gov/component/demoiselle/jsf/restriction/annotation/PredicateGroup.java
@@ -1,5 +1,12 @@
 package br.gov.component.demoiselle.jsf.restriction.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { ElementType.METHOD, ElementType.TYPE, ElementType.FIELD })
 public @interface PredicateGroup {
 	String name();
 
