e6986839e07ee3df410a1e00b4af0f4e679bee5b
jribacruz <jribacruz@gmail.com>
2013-01-07 15:54:22 -0300

    converters

---
+++ demoiselle-restriction-core/src/main/java/br/gov/frameworkdemoiselle/restriction/converters/StringConverter.java
@@ -0,0 +1,19 @@
+package br.gov.frameworkdemoiselle.restriction.converters;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+public class StringConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		return null;
+	}
+
+}
