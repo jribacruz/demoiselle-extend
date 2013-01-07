e687ffcda70508e2009036869478b91c3c828897
jribacruz <jribacruz@gmail.com>
2013-01-07 16:15:03 -0300

    CriteriaController

---
+++ demoiselle-restriction-core/src/main/java/br/gov/frameworkdemoiselle/restriction/stereotype/CriteriaController.java
@@ -0,0 +1,19 @@
+package br.gov.frameworkdemoiselle.restriction.stereotype;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.enterprise.inject.Stereotype;
+import javax.inject.Named;
+
+import br.gov.frameworkdemoiselle.annotation.ViewScoped;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+@Stereotype
+@ViewScoped
+@Named
+public @interface CriteriaController {
+}
