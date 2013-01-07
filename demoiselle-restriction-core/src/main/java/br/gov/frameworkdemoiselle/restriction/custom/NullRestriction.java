de50cea256af4ba7398c333f505d0b58f3ea420c
jribacruz <jribacruz@gmail.com>
2013-01-07 16:05:35 -0300

    custom restrictions

---
+++ demoiselle-restriction-core/src/main/java/br/gov/frameworkdemoiselle/restriction/custom/NullRestriction.java
@@ -0,0 +1,18 @@
+package br.gov.frameworkdemoiselle.restriction.custom;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.apache.commons.lang.StringUtils;
+
+import br.gov.frameworkdemoiselle.restriction.template.RestrictionBean;
+
+public class NullRestriction<T> extends RestrictionBean<T, String> {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Predicate restriction(CriteriaBuilder cb, Root<T> p) {
+		return !StringUtils.isEmpty(getField()) ? cb.isNull(p.get(getField())) : null;
+	}
+}
