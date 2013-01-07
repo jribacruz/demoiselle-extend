f7b2de2e9aedf2f7abf2e1cc86b10b58b5b310a1
jribacruz <jribacruz@gmail.com>
2013-01-07 17:01:55 -0300

    verificacao de tipo

--- demoiselle-restriction-core/src/main/java/br/gov/frameworkdemoiselle/restriction/internal/predicates/RequiredRestrictionPredicate.java
+++ demoiselle-restriction-core/src/main/java/br/gov/frameworkdemoiselle/restriction/internal/predicates/RequiredRestrictionPredicate.java
@@ -3,6 +3,7 @@ package br.gov.frameworkdemoiselle.restriction.internal.predicates;
 import java.lang.reflect.Field;
 
 import br.gov.frameworkdemoiselle.restriction.annotation.Required;
+import br.gov.frameworkdemoiselle.restriction.template.RestrictionBean;
 
 import com.google.common.base.Predicate;
 
@@ -10,7 +11,7 @@ public class RequiredRestrictionPredicate implements Predicate<Field> {
 
 	@Override
 	public boolean apply(Field field) {
-		return field.isAnnotationPresent(Required.class);
+		return RestrictionBean.class.isAssignableFrom(field.getType()) && field.isAnnotationPresent(Required.class);
 	}
 
 }
