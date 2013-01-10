5003fd8dac98262228405ece8c3d9179c4f565ab
jribacruz <jribacruz@gmail.com>
2013-01-09 21:25:04 -0300

    register criteria

--- demoiselle-restriction/src/main/java/br/gov/frameworkdemoiselle/restriction/template/CriteriaBean.java
+++ demoiselle-restriction/src/main/java/br/gov/frameworkdemoiselle/restriction/template/CriteriaBean.java
@@ -3,13 +3,26 @@ package br.gov.frameworkdemoiselle.restriction.template;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.persistence.criteria.CompoundSelection;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Root;
 
+import br.gov.frameworkdemoiselle.restriction.context.CriteriaContext;
+
 public class CriteriaBean<T> implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private CriteriaContext context;
+
+	@SuppressWarnings("unused")
+	@PostConstruct
+	private void init() {
+		context.registerCriteria(this.getClass(), this);
+	}
 
 	public CompoundSelection<T> projection(CriteriaBuilder cb, Root<T> p) {
 		return null;
