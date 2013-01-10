99829118e8b735be1aa3ca61139a2e4d3bd103e7
jribacruz <jribacruz@gmail.com>
2013-01-09 21:25:16 -0300

    registerCriteria method

--- demoiselle-restriction/src/main/java/br/gov/frameworkdemoiselle/restriction/context/CriteriaContext.java
+++ demoiselle-restriction/src/main/java/br/gov/frameworkdemoiselle/restriction/context/CriteriaContext.java
@@ -7,10 +7,10 @@ import br.gov.frameworkdemoiselle.restriction.template.CriteriaBean;
 public interface CriteriaContext extends Serializable {
 	public <T> Class<? extends CriteriaBean<T>> getCriteriaClass();
 
-	public <T> void setCriteriaClass(Class<? extends CriteriaBean<T>> criteriaClass);
+	public <T> void registerCriteriaClass(Class<? extends CriteriaBean<T>> criteriaClass);
 
 	public <T> CriteriaBean<T> getCriteria(Class<?> criteriaClass);
 
-	public <T> void setCriteria(Class<?> criteriaClass, CriteriaBean<T> criteriaBean);
+	public <T> void registerCriteria(Class<?> criteriaClass, CriteriaBean<T> criteriaBean);
 
 }
