4f74adee882bb26d7c555e8648363d730d46d6ee
jribacruz <jribacruz@gmail.com>
2013-01-09 23:06:37 -0300

    criteria persistence

---
+++ demoiselle-restriction/src/main/java/br/gov/frameworkdemoiselle/restriction/persistence/CriteriaPersistence.java
@@ -0,0 +1,39 @@
+package br.gov.frameworkdemoiselle.restriction.persistence;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import br.gov.frameworkdemoiselle.restriction.context.CriteriaContext;
+import br.gov.frameworkdemoiselle.stereotype.PersistenceController;
+import br.gov.frameworkdemoiselle.template.JPACrud;
+import br.gov.frameworkdemoiselle.util.Reflections;
+
+@PersistenceController
+public class CriteriaPersistence<T, I> extends JPACrud<T, I> {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private CriteriaContext context;
+
+	@Override
+	public List<T> findAll() {
+
+		CriteriaBuilder cb = this.getCriteriaBuilder();
+		CriteriaQuery<T> cq = cb.createQuery(getBeanClass());
+		Root<T> p = cq.from(getBeanClass());
+
+		TypedQuery<T> query = this.getEntityManager().createQuery(cq);
+
+		return query.getResultList();
+	}
+
+	@Override
+	protected Class<T> getBeanClass() {
+		return Reflections.<T>getGenericTypeArgument(context.getCriteriaClass(), 0);
+	}
+}
