2b84b9ee4aaf120d2b74fa52cfd89fdadd5548b0
jribacruz <jribacruz@gmail.com>
2013-01-13 12:37:53 -0300

    custom restrictions

---
+++ demoiselle-restriction/src/main/java/br/gov/frameworkdemoiselle/restriction/custom/GreaterThanRestriction.java
@@ -0,0 +1,19 @@
+package br.gov.frameworkdemoiselle.restriction.custom;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.apache.commons.lang.StringUtils;
+
+import br.gov.frameworkdemoiselle.restriction.template.RestrictionBean;
+
+public class GreaterThanRestriction<T, X extends Number> extends RestrictionBean<T, X> {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Predicate restriction(CriteriaBuilder cb, Root<T> p) {
+		return getValue() != null && !StringUtils.isEmpty(getField()) ? cb.gt(p.<Number> get(getField()), getValue()) : null;
+	}
+
+}
