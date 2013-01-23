dd5b4b50ca87d05d0c93cbc575c7ee29786da12a
jribacruz <jribacruz@gmail.com>
2013-01-23 18:45:13 -0300

    JPAProcessor

--- demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction2/producer/LazyDataModelProducer.java
+++ demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction2/producer/LazyDataModelProducer.java
@@ -13,13 +13,24 @@ import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 import org.slf4j.Logger;
 
+import br.gov.frameworkdemoiselle.internal.configuration.PaginationConfig;
 import br.gov.frameworkdemoiselle.restriction2.annotations.CriteriaBy;
+import br.gov.frameworkdemoiselle.restriction2.base.RestrictionMap;
+import br.gov.frameworkdemoiselle.restriction2.processor.JPAProcessor;
 import br.gov.frameworkdemoiselle.restriction2.template.CriteriaBean;
+import br.gov.frameworkdemoiselle.restriction2.template.RestrictionBean;
+import br.gov.frameworkdemoiselle.util.Reflections;
 
 public class LazyDataModelProducer {
 
 	@Inject
 	private Logger logger;
+
+	@Inject
+	private JPAProcessor processor;
+
+	@Inject
+	private PaginationConfig paginationConfig;
 
 	@Produces
 	@CriteriaBy(value = CriteriaBean.class)
@@ -27,7 +38,17 @@ public class LazyDataModelProducer {
 
 		logger.info("Inicializando LazyDataModel para classe {} ", new Object[] { getCriteriaClass(ip.getMember()).getName() });
 
-		return getLazyDataModel(getCriteriaClass(ip.getMember()));
+		// for (Field field :
+		// getCriteriaClass(ip.getMember()).getDeclaredFields()) {
+		// if (RestrictionBean.class.isAssignableFrom(field.getType())) {
+		// RestrictionMap map = new
+		// RestrictionMap(getCriteriaClass(ip.getMember()), field, null);
+		// System.out.println(map);
+		// System.out.println(map.getRestrictionBean());
+		// }
+		// }
+
+		return this.getLazyDataModel(getCriteriaClass(ip.getMember()), this.<T> getBeanClass(ip.getMember()));
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -35,14 +56,24 @@ public class LazyDataModelProducer {
 		return ((Field) member).getAnnotation(CriteriaBy.class).value();
 	}
 
+	private <T> Class<T> getBeanClass(Member member) {
+		return Reflections.<T> getGenericTypeArgument(member, 0);
+	}
+
 	@SuppressWarnings("rawtypes")
-	private <T> LazyDataModel<T> getLazyDataModel(final Class<? extends CriteriaBean> criteriaClass) {
+	private <T> LazyDataModel<T> getLazyDataModel(final Class<? extends CriteriaBean> criteriaClass, final Class<T> beanClass) {
 		return new LazyDataModel<T>() {
 			private static final long serialVersionUID = 1L;
 
 			@Override
 			public List<T> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
-				return null;
+
+				pageSize = pageSize == 0 ? paginationConfig.getPageSize() : pageSize;
+				
+				List<T> list = processor.findAll(beanClass, first, pageSize);
+				this.setRowCount(processor.getRowCount());
+
+				return list;
 			}
 		};
 	}
