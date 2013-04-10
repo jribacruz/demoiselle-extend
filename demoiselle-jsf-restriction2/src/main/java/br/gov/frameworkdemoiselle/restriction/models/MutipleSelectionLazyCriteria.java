e05d8f5b53017c02de47949135c028d625578020
jribacruz <jribacruz@gmail.com>
2013-04-10 20:26:00 -0300

    new constructors

--- demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction/models/MutipleSelectionLazyCriteria.java
+++ demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction/models/MutipleSelectionLazyCriteria.java
@@ -1,8 +1,13 @@
 package br.gov.frameworkdemoiselle.restriction.models;
 
 import br.gov.frameworkdemoiselle.restriction.DefaultLazyCriteria;
+import br.gov.frameworkdemoiselle.restriction.builder.JPABuilder;
 
 public class MutipleSelectionLazyCriteria<T> extends DefaultLazyCriteria<T> {
 	private static final long serialVersionUID = 1L;
-
+	
+	public MutipleSelectionLazyCriteria(Class<T> beanClass, JPABuilder<T> builder) {
+		this.beanClass = beanClass;
+		this.builder = builder;
+	}
 }
--- demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction/models/SingleSelectionLazyCriteria.java
+++ demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction/models/SingleSelectionLazyCriteria.java
@@ -1,7 +1,13 @@
 package br.gov.frameworkdemoiselle.restriction.models;
 
 import br.gov.frameworkdemoiselle.restriction.DefaultLazyCriteria;
+import br.gov.frameworkdemoiselle.restriction.builder.JPABuilder;
 
 public class SingleSelectionLazyCriteria<T> extends DefaultLazyCriteria<T> {
 	private static final long serialVersionUID = 1L;
+	
+	public SingleSelectionLazyCriteria(Class<T> beanClass, JPABuilder<T> builder) {
+		this.beanClass = beanClass;
+		this.builder = builder;
+	}
 }
