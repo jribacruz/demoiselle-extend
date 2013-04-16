a67beae500f7dec34bb5ddab94f1731925853182
jribacruz <jribacruz@gmail.com>
2013-04-15 22:11:18 -0300

    AbstractCriteria

---
+++ demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction/core/AbstractCriteria.java
@@ -0,0 +1,82 @@
+package br.gov.frameworkdemoiselle.restriction.core;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.primefaces.model.SortOrder;
+
+import com.google.common.collect.ArrayListMultimap;
+
+public abstract class AbstractCriteria<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityManager em;
+
+	private Class<T> beanClass;
+
+	protected ArrayListMultimap<SortOrder, String> orderMap = ArrayListMultimap.create();
+
+	protected abstract List<Predicate> getPredicates(CriteriaBuilder cb, Root<T> p);
+
+	protected abstract List<Order> getOrders(CriteriaBuilder cb, Root<T> p);
+
+	public void addOrder(SortOrder order, String value) {
+		this.orderMap.put(order, value);
+	}
+
+	public List<T> findAll(int first, int pageSize) {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<T> cq = cb.createQuery(getBeanClass());
+		Root<T> p = cq.from(getBeanClass());
+
+		List<Predicate> predicates = getPredicates(cb, p);
+		if (predicates != null && !predicates.isEmpty()) {
+			cq.where(predicates.toArray(new Predicate[] {}));
+		}
+
+		List<Order> orders = getOrders(cb, p);
+		if (orders != null && !orders.isEmpty()) {
+			cq.orderBy(orders);
+		}
+
+		TypedQuery<T> query = em.createQuery(cq);
+
+		return query.getResultList();
+	}
+
+	public int countAll() {
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+		Root<T> p = cq.from(getBeanClass());
+
+		cq.select(cb.count(p));
+
+		List<Predicate> predicates = getPredicates(cb, p);
+		if (predicates != null && !predicates.isEmpty()) {
+			cq.where(predicates.toArray(new Predicate[] {}));
+		}
+
+		TypedQuery<Long> query = em.createQuery(cq);
+
+		return query.getSingleResult().intValue();
+	}
+
+	public void setBeanClass(Class<T> beanClass) {
+		this.beanClass = beanClass;
+	}
+
+	public Class<T> getBeanClass() {
+		return beanClass;
+	}
+
+}
--- demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction/core/DefaultLazyModel.java
+++ demoiselle-jsf-restriction2/src/main/java/br/gov/frameworkdemoiselle/restriction/core/DefaultLazyModel.java
@@ -4,38 +4,30 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-import javax.inject.Inject;
-
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 
-import br.gov.frameworkdemoiselle.restriction.builder.JPABuilder;
-import br.gov.frameworkdemoiselle.restriction.context.ModelContext;
-import br.gov.frameworkdemoiselle.restriction.orderer.DataTableOrder;
+import br.gov.frameworkdemoiselle.util.Strings;
 
 public class DefaultLazyModel<T> extends LazyDataModel<T> implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Inject
-	protected JPABuilder<T> builder;
-
-	@Inject
-	private ModelContext context;
-
-	protected Criteria<T> criteria;
+	protected AbstractCriteria<T> criteria;
 
 	@Override
 	public List<T> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
-		context.setDefaultLazyModel(this);
-		this.setRowCount(builder.countAll());
-		return builder.findAll(first, pageSize, new DataTableOrder(sortField, sortOrder));
+		if(!Strings.isEmpty(sortField)) {
+			this.criteria.addOrder(sortOrder, sortField);
+		}
+		this.setRowCount(this.criteria.countAll());
+		return this.criteria.findAll(first, pageSize);
 	}
 
-	public Criteria<T> getCriteria() {
+	public AbstractCriteria<T> getCriteria() {
 		return criteria;
 	}
 
-	public void setCriteria(Criteria<T> criteria) {
+	public void setCriteria(AbstractCriteria<T> criteria) {
 		this.criteria = criteria;
 	}
 
